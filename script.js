85537e73341612c6e524ae14f8849b1aa5f906de
saat12 <guzelkaralarm@gmail.com>
2022-08-05 11:48:46 +0300

    add the second display for multiline operations

--- script.js
+++ script.js
@@ -27,7 +27,9 @@ function operate (num1,num2,oprAdd) {
 
 const digit = document.querySelectorAll(".digit");
 const display = document.querySelector(".display");
+const displayInProg = document.querySelector(".displayInProg");
 const clear = document.querySelector(".ac");
+const decimalBtn = document.querySelector(".decimal");
 const divBtn = document.querySelector(".div")
 const multBtn = document.querySelector(".mult")
 const subtBtn = document.querySelector(".subt")
@@ -36,37 +38,58 @@ const equalsBtn = document.querySelector(".equals")
 
 //populate the digits in display screen 
 function populateValue () {        
+   
     display.textContent=display.textContent+this.value; 
+
 }
+
 digit.forEach(element => {
     element.addEventListener("click",populateValue)
 });
+
+// decimal point needs to be added once only, restrores the value on clear or next operation 
+function populateValueDecimal () {        
+   
+    display.textContent=display.textContent+this.value; 
+    decimalBtn.value=""
+}
+
+decimalBtn.addEventListener("click",populateValueDecimal)
 
 function clearDisplay () {
     display.textContent=""
     firstValue="";
     secondValue="";
     operand="";
+    decimalBtn.value="."
+    displayInProg.textContent=""
 }
 clear.addEventListener("click",clearDisplay);
 
 let firstValue;
 let secondValue;
 let operand;
+
 firstValue="";
 secondValue="";
 operand="";
+
+
 
 function storeValue () {
    if (firstValue==0) {
     window.globalThis =firstValue=display.textContent;
     window.globalThis =operand=this.value;
     display.textContent="";
-    } else {
+    decimalBtn.value="."
+    } 
+    else {
     getResult();
     window.globalThis =firstValue=display.textContent;
     window.globalThis =operand=this.value;
-    display.textContent="";
+    displayInProg.textContent=result;
+    display.textContent=""
+    decimalBtn.value="."
     }
 }
 
@@ -78,9 +101,10 @@ sumBtn.addEventListener("click",storeValue);
 function getResult () {
     
     window.globalThis = secondValue= display.textContent;
-    let result = operate(firstValue,secondValue,operand);
+    window.globalThis= result = operate(firstValue,secondValue,operand);
     display.textContent=result;
-    
+    decimalBtn.value="."
+    displayInProg.textContent="";
 }
 equalsBtn.addEventListener("click",getResult);
 
